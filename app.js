f64d4291c25d27be121238f1c2c87fc1fa53f86b
serdar kaplan <ozserdarkaplan66@gmail.com>
2026-03-05 20:47:27 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:47:08.785Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:47:26.718Z
+Commit: Resolve timezone issues in date handling
