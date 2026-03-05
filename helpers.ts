7a72ce35dd415360d81592af12cb803d403847a7
serdar kaplan <ozserdarkaplan66@gmail.com>
2026-03-05 20:50:06 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:49:31.990Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:50:05.712Z
+Commit: Fix broken links in documentation
