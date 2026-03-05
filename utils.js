e894bc6adc7608749cb8c794683ff2d7ac675c77
serdar kaplan <ozserdarkaplan66@gmail.com>
2026-03-05 20:48:41 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:48:25.886Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:48:40.850Z
+Commit: Resolve null pointer exception in user service
