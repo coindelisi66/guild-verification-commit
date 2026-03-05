30ce028107deb79ad63713c08109a1dbe98c78cf
serdar kaplan <ozserdarkaplan66@gmail.com>
2026-03-05 20:48:26 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:47:44.696Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T17:48:25.886Z
+Commit: Add export functionality for reports
