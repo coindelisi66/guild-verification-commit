3a9122b8a6545a862ca5e460334910d4b5bb4144
serdar kaplan <ozserdarkaplan66@gmail.com>
2026-03-05 20:48:45 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:48:22.971Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T17:48:44.530Z
+Commit: Patch security vulnerability in authentication
