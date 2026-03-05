707cb9491213e498751e83c4464561de1563bafd
serdar kaplan <ozserdarkaplan66@gmail.com>
2026-03-05 20:46:37 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:46:09.130Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T17:46:37.094Z
+Commit: Resolve null pointer exception in user service
